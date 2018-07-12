195fcfe9488c424e09393047f58cc5f09aeed46f
FatDargon <zhujmhh@qq.com>
2018-07-12 21:56:31 +0800

    yes
    777777

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_07_12_073706) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "price", default: 0.0, null: false
   end
 
 end
